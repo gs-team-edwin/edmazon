18e75bb9011a790bc96d800b0cc4d36477fa318a
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-17 16:04:58 -0500

    clean up routes.js

--- client/routes.js
+++ client/routes.js
@@ -22,16 +22,6 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/products/:id" component={AllProducts} />
         <Redirect from="/" to="/products/0" />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
-          </Switch>
-        )}
-        {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
       </Switch>
     )
   }
