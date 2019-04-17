eb010bd8af2b91d107b0796995ae858e66e8e671
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-16 22:56:18 -0500

    big seed script update

--- script/seed.js
+++ script/seed.js
@@ -1,81 +1,289 @@
+/* eslint-disable no-shadow */
 'use strict'
-
 const db = require('../server/db')
-const {User, Review, Product} = require('../server/db/models')
+const {
+  User,
+  Review,
+  Product,
+  Category,
+  Order,
+  ProductsCategories,
+  OrdersProducts
+} = require('../server/db/models')
 const faker = require('faker')
 
-function usersInfo() {
-  let usersArr = []
-  let emailArr = []
-  //{email: asdfasdf, password, googleId, usertype, etc.}
-  //loop n times
-  //generate email
-  //make email
-  //check if email is already in usersArr
-  //if so, go back to step 1
-  //generate password
-  //generate usertype
+// global constants
+const N = 100
+const PHOTO_COUNT = 13
+const CATEGORY_COUNT = 5
 
-  for (let i = 0; i < 600; i++) {
+/**
+ * UTILITY FUNCTIONS
+ */
+
+// includes 0 to max - 1
+function getRandomInteger(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+// capitalizes first letter in word
+function titleCaseWord(word) {
+  if (/^[a-z]/.test(word[0])) {
+    return word[0].toUpperCase() + word.slice(1)
+  } else return word
+}
+// title cases a phrase
+function titleCase(phrase) {
+  return phrase
+    .split(' ')
+    .map(titleCaseWord)
+    .join(' ')
+}
+
+// shuffle array using fisher-yates
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    let j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
+// get random date
+function randomDate(start = new Date(2016, 0, 1), end = new Date()) {
+  let randDate = new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  )
+  return randDate.getTime()
+}
+
+/**
+ * FACTORY FUNCTIONS FOR BASE MODELS
+ */
+
+// build users array
+function userInfoFactory(N) {
+  const usersArr = []
+  const emailArr = []
+  for (let i = 0; i < N; i++) {
+    // loop until we get a unique email
     let email = faker.internet.email()
     while (emailArr.includes(email)) {
       email = faker.internet.email()
     }
     emailArr.push(email)
-    let password = faker.internet.password()
-
+    const password = faker.internet.password()
     usersArr.push({email, password, userType: 'user'})
   }
-  usersArr[0].userType = 'admin'
-  //  usersArr.push(User.create({username: faker.lorem.word(), email: faker.internet.email()}))
-
+  usersArr[0].userType = 'admin' // set the first user to admin
   return usersArr
 }
 
-
-
-
-
-function getRandomInteger(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-
-
-function reviewsInfo () {
-  let reviewsArr = []
-  for (let i = 0; i < 100; i++) {
-    let reviewTitle = faker.hacker.adjective() + ' ' + faker.hacker.noun()
-    let reviewBody = faker.hacker.phrase()
-    let stars = getRandomInteger(5) + 1
-    let productId = getRandomInteger(100) + 1
-    let userId = getRandomInteger(100) + 1
-    reviewsArr.push({title: reviewTitle, body: reviewBody, stars, productId, userId})
+// build categories array
+function categoryFactory(CATEGORY_COUNT) {
+  const categoriesArr = []
+  const categoryWords = shuffle([
+    'Sensors',
+    'Capacitors',
+    'Protocols',
+    'Matrices',
+    'Hard Drives',
+    'Drivers',
+    'Alarms',
+    'Systems',
+    'Microchips',
+    'Monitors'
+  ])
+  for (let i = 0; i < CATEGORY_COUNT; i++) {
+    const name = categoryWords.pop()
+    categoriesArr.push({name})
   }
-  return reviewsArr
+  return categoriesArr
 }
 
-function productsInfo () {
-  let productsArr = []
-  for (let i = 0; i < 100; i++) {
-    let title = faker.hacker.adjective() + ' ' + faker.hacker.noun()
-    let description = faker.hacker.phrase()
-    let price = getRandomInteger(10)
-    productsArr.push({title, description, price})
+// build products array
+function productInfoFactory(N) {
+  const productsArr = []
+  for (let i = 0; i < N; i++) {
+    let title =
+      faker.hacker.adjective() +
+      ' ' +
+      faker.hacker.adjective() +
+      ' ' +
+      faker.hacker.noun()
+    title = titleCase(title)
+    const description =
+      faker.hacker.phrase() +
+      ' ' +
+      faker.hacker.phrase() +
+      ' ' +
+      faker.hacker.phrase()
+    const price = getRandomInteger(1000)
+    const quantityOnHand = getRandomInteger(100)
+    const photoNumber = getRandomInteger(PHOTO_COUNT) + 1
+    const photoUrl = `/images/${photoNumber}.jpg`
+    productsArr.push({
+      title,
+      description,
+      price,
+      quantityOnHand,
+      photoUrl
+    })
   }
   return productsArr
 }
 
+// build reviews array
+function reviewInfoFactory(N) {
+  const reviewsArr = []
+  for (let i = 0; i < N; i++) {
+    const reviewTitle = titleCase(
+      faker.hacker.adjective() + ' ' + faker.hacker.noun()
+    )
+    const reviewBody = faker.hacker.phrase()
+    const stars = getRandomInteger(5) + 1
+    const productId = getRandomInteger(N) + 1
+    const userId = getRandomInteger(N) + 1
+    reviewsArr.push({
+      title: reviewTitle,
+      body: reviewBody,
+      stars,
+      productId,
+      userId
+    })
+  }
+  return reviewsArr
+}
+
+// build orders array
+function orderFactory(N) {
+  let orders = []
+  for (let i = 0; i < N; i++) {
+    const userId = getRandomInteger(N) + 1
+    const sessionId = null
+    const statusChoices = [
+      'cart',
+      'created',
+      'processing',
+      'cancelled',
+      'completed'
+    ]
+    const status = statusChoices[getRandomInteger(4)]
+    const tracking = faker.random.uuid()
+    const checkoutDate = randomDate()
+    orders.push({
+      userId,
+      sessionId,
+      status,
+      tracking,
+      checkoutDate
+    })
+  }
+  return orders
+}
+
+/**
+ * FACTORY FUNCTIONS FOR MANY-TO-MANY TABLES
+ */
+
+// assign all products to a category
+// and give some products multiple categories
+function productCategoryFactory(N, CATEGORY_COUNT) {
+  const productCategoryArr = []
+  for (let i = 0; i < N; i++) {
+    // make sure every product is in a category
+    const productId = i + 1
+    let categoryId = i % CATEGORY_COUNT + 1
+    productCategoryArr.push({productId, categoryId})
+
+    // randomly add 1 more category to some products
+    let chance = getRandomInteger(100)
+    if (chance < 50) {
+      const numberToAdd = getRandomInteger(3)
+      for (let x = 0; x < numberToAdd; x++) {
+        categoryId = (categoryId + 1) % CATEGORY_COUNT + 1
+        productCategoryArr.push({productId, categoryId})
+      }
+    }
+  }
+  return productCategoryArr
+}
+
+// map products to orders
+// every order gets between one and five products
+async function ordersProductsFactory(N) {
+  const ordersProductsArr = []
+
+  // loop through N
+  for (let i = 0; i < N; i++) {
+    const numberOfProducts = getRandomInteger(4) + 1
+
+    // set the first product id
+    let productId = getRandomInteger(100) + 1
+
+    // each loop adds a product
+    for (let x = 0; x < numberOfProducts; x++) {
+      // set up the easy stuff
+      const orderId = i + 1
+      productId = (productId + 1) % 100 + 1 // make sure we don't duplicate products
+      const quantity = getRandomInteger(4) + 1
+
+      // get the purchased status for this order
+      // if it's cart, set price to null
+      // else, 50% chance price has changed
+      const orderObject = await Order.findByPk(orderId)
+      const orderStatus = orderObject.dataValues.status
+      let purchasePrice
+      if (orderStatus === 'cart') {
+        purchasePrice = null
+      } else {
+        let priceChangeChance = getRandomInteger(100)
+        if (priceChangeChance < 50) {
+          const productObject = await Product.findByPk(productId)
+          purchasePrice = productObject.dataValues.price
+        } else {
+          purchasePrice = getRandomInteger(1000)
+        }
+      }
+      ordersProductsArr.push({orderId, productId, quantity, purchasePrice})
+    }
+  }
+  return ordersProductsArr
+}
+
+/**
+ * SEEDING FUNCTIONS
+ */
+
+// call everything and do the seeding
 async function seed() {
+  // clear the db
   await db.sync({force: true})
   console.log('db synced!')
 
-  const users = await User.bulkCreate(usersInfo())
-  await Product.bulkCreate(productsInfo())
-  await Review.bulkCreate(reviewsInfo())
-
-
+  const users = await User.bulkCreate(userInfoFactory(N))
   console.log(`seeded ${users.length} users`)
+
+  const categories = await Category.bulkCreate(categoryFactory(CATEGORY_COUNT))
+  console.log(`seeded ${categories.length} categories`)
+
+  const products = await Product.bulkCreate(productInfoFactory(N))
+  console.log(`seeded ${products.length} produdcts`)
+
+  const productCategory = await ProductsCategories.bulkCreate(
+    productCategoryFactory(N, CATEGORY_COUNT)
+  )
+  console.log(`seeded ${productCategory.length} productCategory rows`)
+
+  const reviews = await Review.bulkCreate(reviewInfoFactory(N))
+  console.log(`seeded ${reviews.length} reviews`)
+
+  const orders = await Order.bulkCreate(orderFactory(N))
+  console.log(`seeded ${orders.length} orders`)
+
+  const ordersProducts = await OrdersProducts.bulkCreate(
+    await ordersProductsFactory(N)
+  )
+  console.log(`seeded ${ordersProducts.length} ordersProducts rows`)
+
   console.log(`seeded successfully`)
 }
 
