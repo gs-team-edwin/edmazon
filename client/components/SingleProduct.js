b5639dbae307e72bbca5b953cd3f1dc5560f18ab
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-04-17 23:19:59 -0500

    Helvey worked on AllProducts/Single Link

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {getAllProducts} from '../store/products'
+import {getSingleProduct} from '../store/products'
 
 class SingleProduct extends Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
+    console.log(this.props)
     return <div />
   }
 }
 
-export default SingleProduct
+const mapStateToProps = state => {
+  return {
+    products: state.products
+  }
+}
+
+// const mapDispatch = dispatch => {
+//   return {
+//
+//   }
+// }
+
+export default connect(mapStateToProps)(SingleProduct)
