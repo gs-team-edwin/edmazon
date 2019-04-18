7f481ac028df652a0579dc1a82fe0b6467ae1230
Barry <gnyrrab@gmail.com>
2019-04-18 14:54:55 -0500

    single product and categories

---
+++ client/components/navcategorybar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import history from '../history'
+import {connect} from 'react-redux'
+import {getProductByCategory} from '../store/products'
+import {getAllCategories} from '../store/categories'
+
+class NavCategoryBar extends React.Component {
+  componentDidMount() {
+    this.props.gotAllCategories()
+  }
+  render() {
+    let categories = this.props.categories
+    return (
+      <div className="navbar">
+        <nav className="navbar-right-box">
+            <span className="navbar-link-container">
+            {categories.map(category => (
+              <button type="button" onClick={() => this.props.gotAllProducts(category.id,0)}>
+              Category {category.id}
+            </button>
+            ))}
+            </span>
+        </nav>
+      </div>
+    )
+  }
+}
+const mapStateToProps = state => {
+  return {
+    categories: state.categories
+  }
+}
+const mapDispatch = dispatch => {
+  return {
+    gotAllProducts: (category, offset) => dispatch(getProductByCategory(category, offset)),
+    gotAllCategories: ()=>dispatch(getAllCategories())
+  }
+}
+export default connect(mapStateToProps, mapDispatch)(NavCategoryBar)
+
+/**
+ * CONTAINER
+ */
