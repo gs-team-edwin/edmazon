4b1dd9e1a67bf8e410a51576c614dd0701752587
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-04-18 15:05:03 -0500

    post

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Product} = require('../db/models')
+const {Product, Review} = require('../db/models')
 const {Op} = require('sequelize')
 module.exports = router
 
@@ -25,6 +25,15 @@ router.get('/:id', async (req, res, next) => {
     let id = req.params.id
     const products = await Product.findbyPK(id)
     res.json(products)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/:id', async (req, res, next) => {
+  try {
+    console.log(req.body)
+    const review = await Review.create(req.body)
   } catch (err) {
     next(err)
   }
