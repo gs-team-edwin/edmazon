073a07e3c63ad8dfaa515910c64da0a73496c451
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-20 21:37:35 -0500

    wire up links

--- client/components/SmallProductCard.js
+++ client/components/SmallProductCard.js
@@ -4,15 +4,18 @@ import {Link} from 'react-router-dom'
 
 const SmallProductCard = props => {
   const {product} = props
-  // `/product/${product.id}`
+  const productLink = () => history.push(`/product/${product.id}`)
   return (
     <div className="small-product-card ">
       <img
+        onClick={productLink}
         className="small-product-card-image"
         src={`${product.photos[0].photoUrl}`}
       />
       <div className="small-product-card-right">
-        <div className="small-product-card-title">{product.title}</div>
+        <div onClick={productLink} className="small-product-card-title">
+          {product.title}
+        </div>
         <div className="small-product-card-price">${product.price / 100}</div>
         <div className="small-product-card-description">
           {product.description}
@@ -21,7 +24,7 @@ const SmallProductCard = props => {
         <div className="small-product-card-categories">
           {product.categories.map(cat => (
             <button
-              key={cat.div}
+              key={cat.id}
               className="small-category-link"
               type="button"
               onClick={() =>
