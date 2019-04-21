59570e0ebcb24e8b0420916628bed992e468b107
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-04-21 18:32:02 -0500

    CHange Reviews To Popup, Exrtact ReviewsCard Component, extracted SingleProduct Compenent Card.

--- client/components/ReviewForm.js
+++ client/components/ReviewForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {writeReview} from '../store/reviews'
+import {writeReview, closePopup} from '../store'
 //update order History with addreses
 
 //STILL NEEDS WORK
@@ -20,14 +20,14 @@ class ReviewForm extends React.Component {
   async handleSubmit(event) {
     try {
       event.preventDefault()
-      await this.props.writeReview(this.props.match.params.id, {
+      await this.props.writeReview(this.props.productId, {
         stars: this.state.stars,
         title: this.state.title,
         body: this.state.body,
-        productId: Number(this.props.match.params.id),
+        productId: Number(this.props.productId),
         userId: 10
       })
-      this.props.history.push(`/product/${this.props.match.params.id}`)
+      this.props.closePopup()
     } catch (err) {
       console.log(err)
     }
@@ -41,48 +41,53 @@ class ReviewForm extends React.Component {
   render() {
     const {stars, title, body} = this.state
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h3>Review Products</h3>
-        <div>
-          <label htmlFor="name">STARS:</label>
-          <input
-            type="text"
-            name="stars"
-            onChange={this.handleChange}
-            value={stars}
-          />
-          <label htmlFor="title">Title:</label>
-          <input
-            required
-            type="text"
-            name="title"
-            onChange={this.handleChange}
-            value={title}
-          />
-        </div>
-        <div>
-          <label htmlFor="body">review:</label>
-          <input
-            required
-            type="text"
-            name="body"
-            onChange={this.handleChange}
-            value={body}
-          />
-        </div>
+      <div className="popup-outer-container">
+        <div className="popup">
+          <form onSubmit={this.handleSubmit}>
+            <h3>Review Products</h3>
+            <div>
+              <label htmlFor="name">STARS:</label>
+              <input
+                type="text"
+                name="stars"
+                onChange={this.handleChange}
+                value={stars}
+              />
+              <label htmlFor="title">Title:</label>
+              <input
+                required
+                type="text"
+                name="title"
+                onChange={this.handleChange}
+                value={title}
+              />
+            </div>
+            <div>
+              <label htmlFor="body">review:</label>
+              <input
+                required
+                type="text"
+                name="body"
+                onChange={this.handleChange}
+                value={body}
+              />
+            </div>
 
-        <div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
+            <div>
+              <div>
+                <button type="submit">Submit</button>
+              </div>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     )
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  writeReview: (userId, review) => dispatch(writeReview(userId, review))
+  writeReview: (userId, review) => dispatch(writeReview(userId, review)),
+  closePopup: () => dispatch(closePopup())
 })
 
 export default connect(null, mapDispatchToProps)(ReviewForm)
