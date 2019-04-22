9399579e99aa9c4e1bc71c1c82142085fc5a69d1
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-21 22:18:29 -0500

    change structure of order item

--- client/components/OrderItem.js
+++ client/components/OrderItem.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-
-const orderItem = (props) => {
-    return (
-        <div className="cart-products">
-            <Link to={`/product/${props.product.id}`}>{props.product.title}</Link>
-            <div> ${props.product.price}</div>
-            <button
-                className="popup-close-button"
-                type="button"
-                onClick={() => props.removeItem(props.product.id)}
-            > Close </button>
-        </div>
-    )
+const orderItem = props => {
+  return (
+    <div className="cart-product">
+      <Link to={`/product/${props.product.id}`}>{props.product.title}</Link>
+      <div> ${props.product.price}</div>
+      <button
+        className="remove-item-button"
+        type="button"
+        onClick={() => props.removeItem(props.product.id)}
+      >
+        Remove from cart
+      </button>
+    </div>
+  )
 }
 
 export default orderItem
