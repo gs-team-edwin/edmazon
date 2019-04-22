9c8788ed99b2c2980adae48844ad280f82dc16c5
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-22 13:50:41 -0500

    fix bug in qty selector

--- client/components/LargeProductCard.js
+++ client/components/LargeProductCard.js
@@ -101,7 +101,7 @@ class LargeProductCard extends React.Component {
                 >
                   <option value="1">1</option>
                   <option value="2">2</option>
-                  <option value="3">4</option>
+                  <option value="3">3</option>
                   <option value="4">4</option>
                   <option value="5">5</option>
                 </select>
