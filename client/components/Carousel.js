ae250a7088efd6afd01d026f84595e8e3cf27429
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-04-23 16:35:14 -0500

    CaroselFinished

--- client/components/Carousel.js
+++ client/components/Carousel.js
@@ -1,21 +1,62 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Carousel = props => {
-  return (
-    <div>
-      <div className="pagination-container">
-        <button type="button" className="pagination-button">
-          ◀
-        </button>
-        <img className="carousel-img" src="/images/EdwinBanner.jpg" />
-        <img src="/images/amazon-black-icon.jpg" />
-        <img className="carousel-img" src="/images/5StarCodingClasses.jpg" />
-        <button type="button" className="pagination-button">
-          ▶
-        </button>
+const images = [
+  '/images/amazon-black-icon.jpg',
+  '/images/EdwinChange.jpg',
+  '/images/5StarCodingClasses.jpg'
+]
+
+class Carousel extends Component {
+  constructor() {
+    super()
+    this.state = {
+      image: '',
+      curIdx: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    let nextIdx = this.state.curIdx + 1
+    if (images[nextIdx] == null) {
+      this.setState(state => ({
+        image: images[0],
+        curIdx: 0
+      }))
+    } else {
+      this.setState(state => ({
+        image: images[nextIdx],
+        curIdx: nextIdx
+      }))
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="pagination-container">
+          <button
+            onClick={this.handleClick}
+            type="button"
+            className="pagination-button"
+          >
+            ◀
+          </button>
+          {!this.state.image ? (
+            <img className="carousel-img" src="/images/amazon-black-icon.jpg" />
+          ) : (
+            <img className="carousel-img" src={this.state.image} />
+          )}
+          <button
+            onClick={this.handleClick}
+            type="button"
+            className="pagination-button"
+          >
+            ▶
+          </button>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Carousel
