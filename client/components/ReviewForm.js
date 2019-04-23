e6d798c4dc16b377abe69ae1410f1cdf15be1b57
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-23 15:28:22 -0500

    remove import

--- client/components/ReviewForm.js
+++ client/components/ReviewForm.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Redirect} from 'react-router'
 import {writeReview, closePopup} from '../store'
 //update order History with addreses
 
